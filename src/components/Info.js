58c33a22e7acf05312d8a8174604bf7edad4840b
benjamin-summers <summerstyler2@gmail.com>
2018-12-09 17:30:46 -0500

    added pokebal image

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+const imgStyles = {
+    height: 25 + 'px',
+    width: 25 + 'px'
+}
+
 const Info = ({pokeName, info, Class}) => {
     return (
         <div className={Class}>
             <div>
                 <p>{pokeName}</p>
+            </div>
+            <div>
+                <img style={imgStyles} src="https://i.imgur.com/jztFqA0.jpg" alt="pokeball" />
             </div>
             <div>
                 {info}
